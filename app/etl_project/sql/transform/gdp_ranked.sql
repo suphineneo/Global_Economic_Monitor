5633234826ddeac209adfad8fa39fdd284b22292
Su Phine <neosuphine@gmail.com>
2024-10-07 18:38:38 +0800

    corrected line 6

--- app/etl_project/sql/transform/gdp_ranked.sql
+++ app/etl_project/sql/transform/gdp_ranked.sql
@@ -3,7 +3,7 @@ select
     country_code,
     country_name,
     region,
-    value as "exports",
+    value as "gdp",
     avg(value) over (partition by year) as avg_gdp_by_year,
     avg(value) over (partition by region) as avg_gdp_by_region,
     rank() over (partition by year order by value desc) as rank_gdp_by_year,
